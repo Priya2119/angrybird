817655aa3b1da4fa6734c00a9582101edba593fd
Priya2119 <priya.nagpal19@gmail.com>
2019-12-21 18:42:54 +0530

    test1

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +17,8 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
+    constrainedLog = new Log(230,180,80,PI/2);
 
 
     ground = new Ground(600,height,1200,20);
@@ -35,6 +40,15 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    var options = {
+        bodyA: bird.body,
+        bodyB: constrainedLog.body,
+        stiffness: 0.04,
+        length: 10
+    }
+     var chain = Constraint.create(options);
+        World.add(world, chain);
 
 }
 
@@ -61,4 +75,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    strokeWeight(4);
+    line(bird.body.position.x, bird.body.position.y, constrainedLog.body.position.x, constrainedLog.body.position.y);
 }
